6b62607f38aa7dee3a7dd76dddffae1e0da1eea1
hyatchou <hyatchou@nalie-PC.tlt.tandemlaunchtech.com>
2011-12-23 11:21:24 -0500

    Author name

--- Ogama/Modules/Recording/MirametrixInterface/MessageReceivedEventArgs.cs
+++ Ogama/Modules/Recording/MirametrixInterface/MessageReceivedEventArgs.cs
@@ -8,8 +8,8 @@
 // You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 // **************************************************************
 // </copyright>
-// <author>Martin Werner</author>
-// <email>martin.werner@alea-technologies.de</email>
+// <author>Herval Yatchou</author>
+// <email>herval.yatchou@tandemlaunchtech.com</email>
 using System.Collections.Generic;
 namespace Ogama.Modules.Recording.MirametrixInterface
 {
