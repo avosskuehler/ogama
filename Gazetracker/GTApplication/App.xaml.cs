ead02ca65472ea5de2c68f8cc56812f74e48cd34
AVosskuehler <AVosskuehler@localhost>
2012-01-08 21:28:53 +0100

    BUGFIX: GTApplication could not be launched. BUGFIX: DummyWindow for Thorlabs now is (invisibly) shown to have a handle. BUGFIX: Hang up on close down of network connection to gazetracker.

--- Gazetracker/GTApplication/App.xaml.cs
+++ Gazetracker/GTApplication/App.xaml.cs
@@ -7,11 +7,11 @@ namespace GTApplication
   using log4net;
 
   /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
-        private static readonly bool isDebugEnabled = log.IsDebugEnabled;
-    }
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application
+  {
+    private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+    private static readonly bool isDebugEnabled = log.IsDebugEnabled;
+  }
 }
