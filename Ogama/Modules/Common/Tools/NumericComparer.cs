6a05bbac40f1a951bec3368c633520dccfb6f73f
AVosskuehler <AVosskuehler@localhost>
2012-03-19 21:36:14 +0100

    IMPROVED: Added dock and center fields to layout section of slide design module which where formerly only in context menu. Added some missing source descriptions.

--- Ogama/Modules/Common/Tools/NumericComparer.cs
+++ Ogama/Modules/Common/Tools/NumericComparer.cs
@@ -6,10 +6,22 @@ using System.IO;
 
 namespace Ogama.Modules.Common.Tools
 {
-
+  /// <summary>
+  /// This class implements <see cref="IComparer"/> for a numeric file name comparison.
+  /// </summary>
   public class NumericComparer : IComparer
   {
-
+    /// <summary>
+    /// Performs a case-sensitive comparison of two objects of the same type and returns a 
+    /// value indicating whether one is less than, equal to, or greater than the other.
+    /// </summary>
+    /// <param name="x">The first object to compare. </param>
+    /// <param name="y">The second object to compare. </param>
+    /// <returns>A signed integer that indicates the relative values of x and y
+    /// Less than zero if x is less than y. 
+    /// Zero if x equals y. 
+    /// Greater than zero if x is greater than y.
+    /// </returns>
     public int Compare(object x, object y)
     {
       if ((x is string) && (y is string))
