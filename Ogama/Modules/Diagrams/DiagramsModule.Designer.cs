9c398f9e2f6b1cddadea6be4790e88e1f7d95eb2
Sergiu <Sergiu@localhost>
2012-03-29 17:14:49 +0300

    - Make the content inside Diagrams module window resize with the window (note this doesn't work too smooth because of winforms/wpf interop) - Made the Custom Chart tab the default one - Designed the content of the Custom Chart tab

--- Ogama/Modules/Diagrams/DiagramsModule.Designer.cs
+++ Ogama/Modules/Diagrams/DiagramsModule.Designer.cs
@@ -51,6 +51,9 @@
             // 
             // elementHost1
             // 
+            this.elementHost1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.elementHost1.Location = new System.Drawing.Point(12, 12);
             this.elementHost1.Name = "elementHost1";
             this.elementHost1.Size = new System.Drawing.Size(550, 450);
