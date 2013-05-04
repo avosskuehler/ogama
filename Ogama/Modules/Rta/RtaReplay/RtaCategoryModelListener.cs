63c7104667698a429573b4bec3d6c9004568601d
rbusjahn <rbusjahn@localhost>
2013-05-04 23:07:05 +0200

    ProgressDialog adjusted

---
+++ Ogama/Modules/Rta/RtaReplay/RtaCategoryModelListener.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ogama.Modules.Rta.RtaReplay
+{
+    interface RtaCategoryModelListener
+    {
+        void onEventAdded(RtaEvent rtaEvent);
+
+        void onEventChanged(RtaEvent rtaEvent);
+        
+        void onEventRemoved(RtaEvent rtaEvent);
+
+        void onCategoryAdded(RtaCategory rtaCategory);
+
+        void onCategoryChanged(RtaCategory rtaCategory);
+
+        void onCategoryRemoved(RtaCategory rtaCategory);
+
+    }
+}
