31a269f147e57681c0506c0800d24136ea36e092
rbusjahn <rbusjahn@localhost>
2013-06-19 11:32:38 +0200

    RtaModule integration

---
+++ OgamaTestProject/DataSet/QueriesTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OgamaTestProject.DataSet
+{
+    [TestClass]
+    public class QueriesTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+           
+        }
+    }
+}
