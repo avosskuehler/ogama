6ec02de1aa24df8c31c62e7dcafb70be2983fbb5
rbusjahn <rbusjahn@localhost>
2013-06-28 16:38:08 +0200

    RtaModel adjusted

--- OgamaDaoTestProject/Dao/Rta/RtaEventDaoTest.cs
+++ OgamaDaoTestProject/Dao/Rta/RtaEventDaoTest.cs
@@ -26,8 +26,7 @@ namespace OgamaDaoTestProject.Dao.Rta
             
             
             RtaCategory category1 = new RtaCategory();
-            RtaCategoryDao categoryDao = new RtaCategoryDao();
-            categoryDao.SetSessionFactory(base.sfh.getHibernateSessionFactory());
+            RtaCategoryDao categoryDao = GetRtaCategoryDao();
             categoryDao.save(category1);
 
             RtaEvent event1 = new RtaEvent();
@@ -41,6 +40,53 @@ namespace OgamaDaoTestProject.Dao.Rta
             Assert.IsNotNull(event2);
             Assert.AreEqual(event1.ID, event2.ID);
 
+        }
+
+        private RtaCategoryDao GetRtaCategoryDao()
+        {
+            RtaCategoryDao categoryDao = new RtaCategoryDao();
+            categoryDao.SetSessionFactory(base.sfh.getHibernateSessionFactory());
+            return categoryDao;
+        }
+
+        [TestMethod]
+        public void TestFindByRtaCategory()
+        {
+            RtaCategoryDao rtaCategoryDao = GetRtaCategoryDao();
+            RtaEventDao cut = GetRtaEventDao();
+
+            RtaCategory cat1 = new RtaCategory();
+            rtaCategoryDao.save(cat1);
+
+            RtaEvent rtaEvent = new RtaEvent();
+            rtaEvent.fkRtaCategory = cat1;
+            cut.save(rtaEvent);
+
+            List<RtaEvent> list = cut.findByRtaCategory(cat1);
+
+            Assert.IsNotNull(list);
+            Assert.IsTrue(list.Count > 0);
+            foreach (RtaEvent rtaEventFound in list)
+            {
+                Assert.AreEqual(rtaEventFound.fkRtaCategory.ID, cat1.ID);
+            }
+        }
+
+        private RtaEventDao GetRtaEventDao()
+        {
+            RtaEventDao dao = new RtaEventDao();
+            dao.SetSessionFactory(base.sfh.getHibernateSessionFactory());
+            return dao;
+        }
+
+
+        [TestMethod]
+        public void TestFindAll()
+        {
+            cut.save(new RtaEvent());
+            List<RtaEvent> list = ((RtaEventDao)cut).findAll();
+            Assert.IsNotNull(list);
+            Assert.IsTrue(list.Count > 0);
         }
     }
 }
