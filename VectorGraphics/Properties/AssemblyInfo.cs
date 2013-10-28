13f8888a86117ee5a92e44891c2736bcc61199ac
avosskuehler <avosskuehler@localhost>
2013-10-28 20:52:43 +0100

    Setup updates for 4.4 release.

--- VectorGraphics/Properties/AssemblyInfo.cs
+++ VectorGraphics/Properties/AssemblyInfo.cs
@@ -43,5 +43,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("4.3.*")]
-[assembly: AssemblyFileVersion("4.3")]
+[assembly: AssemblyVersion("4.4.*")]
+[assembly: AssemblyFileVersion("4.4")]
